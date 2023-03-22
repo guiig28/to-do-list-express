3f51eb2a6bd210243b88e64b77970eec93a6286b
Guilherme Cardoso <gui.cardoso.28@gmail.com>
2023-03-22 15:53:40 -0300

    Instalação e utilização do mongoose

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -2,28 +2,55 @@ const express =  require('express')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  console.log('Hello, world!')
-  res.send()
+const Checklist = require('../models/checklist')
+
+router.get('/', async (req, res) => {
+  try {
+    let checklists = await Checklist.find({})
+    res.status(200).json(checklists)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 })
 
-router.post('/', (req, res) => {
-  console.log(req.body)
-  res.status(200).json(req.body)
+router.post('/', async (req, res) => {
+  let { name } = req.body
+
+  try {
+    let checklist = await Checklist.create({ name })
+    res.status(200).json(checklist)
+  } catch (err) {
+    res.status(422).json(err)
+  }
+
 })
 
-router.get('/:id', (req, res) => {
-  console.log(req.params.id)
-  res.send(`ID: ${req.params.id}`)
+router.get('/:id', async (req, res) => {
+  try {
+    let checklist = await Checklist.findById(req.params.id)
+    res.status(200).json(checklist)
+  } catch (err) {
+    res.status(422).json(err)
+  }
 })
 
-router.put('/:id', (req, res) => {
-  console.log(req.params.id)
-  res.send(`ID: ${req.params.id}`)
+router.put('/:id', async (req, res) => {
+  let { name } = req.body
+
+  try {
+    let checklist = await Checklist.findByIdAndUpdate(req.params.id, {name}, {new: true})
+    res.status(200).json(checklist)
+  } catch (err) {
+    res.status(422).json(err)
+  }
 })
 
-router.delete('/:id', (req, res) => {
-  console.log(req.params.id)
-  res.send(`ID: ${req.params.id}`)
+router.delete('/:id', async (req, res) => {
+try {
+  let checklist = await Checklist.findByIdAndRemove(req.params.id)
+  res.status(200).json(checklist)
+} catch (err) {
+  res.status(422).json(err)
+}
 })
 module.exports = router
